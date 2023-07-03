2fc72a8faa96be189a8835aca50fe8a5bcc79327
f4t1hsalih <f4t1hsalih@gmail.com>
2023-07-03 13:56:39 +0300

    Add project files.

---
+++ Ortalama_Hesaplama/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ortalama_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Öğrenci ort = new Öğrenci();
+            ort.Tckimlikno = textBox4.Text;
+            ort.Ad = textBox6.Text;
+            ort.Soyad = textBox5.Text;
+            MessageBox.Show(ort.Durum(double.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text)));
+        }
+    }
+}
---
+++ Ortalama_Hesaplama/Öğrenci.cs
@@ -0,0 +1,58 @@
+﻿namespace Ortalama_Hesaplama
+{
+    class Öğrenci
+    {
+        private string ad;
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+
+            set
+            {
+                ad = value;
+            }
+
+        }
+
+        private string soyad;
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+
+            set
+            {
+                soyad = value;
+            }
+        }
+
+        private string tckimlikno;
+        public string Tckimlikno
+        {
+            get
+            {
+                return tckimlikno;
+            }
+
+            set
+            {
+                tckimlikno = value;
+            }
+        }
+
+        double ortalama;
+        public string Durum(double not1, double not2, double not3)
+        {
+            string sonuç;
+            ortalama = (not1 + not2 + not3) / 3;
+            if (ortalama >= 50) sonuç = Ad + " " + Soyad + " " + Tckimlikno + "\n" + "Geçti , Ortalaması: " + ortalama;
+            else sonuç = Ad + " " + Soyad + " " + Tckimlikno + "\n" + "Kaldı , Ortalaması: " + ortalama; ;
+            return sonuç;
+        }
+    }
+}
